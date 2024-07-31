34aad018e405dac8e1f17a9c57b35b57e66b82d3
danieltonad <soladan2000@gmail.com>
2024-07-30 15:57:11 -0900

    done?

--- jobs/update_stock.py
+++ jobs/update_stock.py
@@ -6,7 +6,7 @@ from apscheduler.triggers.cron import CronTrigger
 def cron_execute():
     scheduler = BackgroundScheduler()
     
-    scheduler.add_job(func=fetch_stocks_data, trigger=CronTrigger(minute=50))
+    scheduler.add_job(func=fetch_stocks_data, trigger=CronTrigger(minute=2))
     scheduler.start()
     
     for job in scheduler.get_jobs():
